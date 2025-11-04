4b938dae9f11135f8e0e11fa2bd37c80ed2080fa
Shane Darilek <wsehsatnoen@Shanes-MacBook-Pro.local>
2025-11-04 14:38:01 -0600

    updated vite to properly handle file

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.[jt]sx?$/,
+    exclude: [],
+  },
 })
